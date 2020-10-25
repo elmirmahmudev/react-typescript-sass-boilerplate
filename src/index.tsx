4905569a15381eb9c51f3af835eaef0e717a1e1d
Elmir S. Mahmudov <elmirmahmudev@gmail.com>
2020-10-26 00:13:05 +0400

    node-sass added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
+import './index.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <h1>Hello World!</h1>
